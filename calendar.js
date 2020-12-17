fc214e954302bf57facd05270526dd549ee10278
tomaDesign <75678627+tomaDesign@users.noreply.github.com>
2020-12-17 19:02:14 +0100

    ujak

---
+++ calendar.js
@@ -0,0 +1,72 @@
+function elseje(){
+    window.alert("Karácsony fényei, jöjjetek,\nszívünket békesség töltse meg:\nszeretnénk boldognak látszani,\ns lehetnénk egymásnak társai.\n \n  Balogh József");
+}
+function ketto(){
+    window.alert("A karácsony nem fog csak úgy a semmiből bekopogtatni és ránk köszönteni, nekünk kell tennünk azért, hogy megteremtsük magunkban is az ünnepi hangulatot. \n \nMolnár Emese");
+}
+function harom(){
+    window.alert("Butaság lenne (...) a régi karácsonyok hangulatát keresni, mert az már nem fog visszatérni. Új hagyományokat kell teremteni, amiket ugyanúgy a magunkénak érezhetünk családilag.\n \nMolnár Emese");
+}
+function negy(){
+    window.alert("Gyerekként a karácsonyi időszak maga a csoda, csúcspontja pedig az ajándékbontogatás a fa alatt. Aztán az ember felnő, és egy kis szürke réteg rakódik az ünnepekre. \n \nPálúr Krisztina");
+}
+function ot(){
+    window.alert("Túl vagyunk ingerelve, mire eljutunk a karácsonyig, telítődik vele az ember. (...) Nem a temérdek ajándéktól vagy a százféle süteménytől lesz meghitt a karácsony - a kevesebb is elég.\n \nMolnár Emese");
+}
+function hat(){
+    window.alert("Ne engedjen a nyomásnak: a karácsonynak nem kell olyannak lennie, mint a filmekben.\n \nCzeglédi Fanni");
+}
+function het(){
+    window.alert("A karácsony (...) a gyermekek ünnepe - az övék és értük van, legyenek akárhány évesek.\n\nWilliam Paul Young");
+}
+function nyolc(){
+    window.alert("A karácsony nem lenne karácsony ajándékok nélkül.\n\nLouisa May Alcott");
+}
+function kilenc(){
+    window.alert("Ha minden hazugság volna a világon, a karácsonyfának akkor is valódinak kellene lennie.\n\nSztrakoniczky Károly");
+}
+function tiz(){
+    window.alert("A karácsony legmeghatóbb ajándékai azok, amelyeket gyermekektől kapunk, akik azokat majdnem kivétel nélkül sajátmaguk készítik. Ők az igazi szívbeli ajándékozók.\n\nKaesz Gyula");
+}
+function tegy(){
+    window.alert("Karácsony az az időszak, amikor még boldog születésnapot is karácsonyi énekek formájában kívánunk.\n\nCarmen Richardson Rutlen");
+}
+function tketto(){
+    window.alert("A karácsony boldogsága a remény, szelleme a béke és szíve a szeretet. Nem pediglen folytonos fogyasztás, amikor az ez évi ajándokokat a jövő évi pénzből megvesszük.\n\nTemesi Ferenc");
+}
+function tharom(){
+    window.alert("A decemberben leggyakrabban elhangzó beváltatlan ígéret: karácsony előtt még összefutunk.\n\nSzeifert Natália");
+}
+function tnegy(){
+    window.alert("Karácsony éjszakája a legcsöndesebb idő, ami létezhet. Mintha emlékekből lenne az egész világ.\n\nAva Dellaira");
+}
+function tot(){
+    window.alert("Karácsony van. Az örök remény ünnepe.\n\nReszkessetek betörők c. film");
+}
+function that(){
+    window.alert("Az egyedüllét nagyon rossz karácsonykor. Az ünnep érzése nekem: sok ember együtt.\n\nLiptai Claudia");
+}
+function thet(){
+    window.alert("„Ne a hóban, csillagokban, ne ünnepi foszlós kalácson, ne díszített fákon, hanem a szívekben legyen karácsony!”\n\nSzilágyi Domokos");
+}
+function tnyolc(){
+    window.alert("„Háztól házig, szívtől szívig, az egyik helytől a másikig – a karácsony melege és élvezete mindannyiunkat közelebb hoz egymáshoz.”\n\nEmily Matthews");
+}
+function tkilenc(){
+    window.alert("„Ne feledd, ha a karácsony hiányzik a szívedből, akkor a fa alatt sem találsz rá!”\n\nCharlotte Carpenter");
+}
+function husz(){
+    window.alert("A szívem mélyén mindig karácsony van. A szívemben rejtegetett zug, ahova visszajárok örülni, maga a karácsony.\n\nSzabó T. Anna");
+}
+function hegy(){
+    window.alert("Már csak pár nap van hátra karácsonyig, az emberek átlagos térfogata kétszeresére nőtt az ajándékoktól. És mintha versenyt cipekednének, minden egyes csomag azt üzeni: az én terhem a legnagyobb. \n \n  Mécs Anna");
+}
+function hketto(){
+    window.alert("Amig hisz­tek a karácsony ünnepben, az év e legszebb óráiban, addig bárhol lesztek is, mindig terítve fog várni benneteket a karácsonyünnep asztal­kája.\n\nCinkotai Nagy Ince");
+}
+function hharom(){
+    window.alert("Ahány ember, annyi karácsony.\n\nKatie Fforde");
+}
+function hnegy(){
+    window.alert("Karácsonyest, nyugalmas este,\nszemünk a friss havon mereng\nés lelkünk bánattal övezve\nki tudja merre, merre leng?\n\nSzabó Lőrinc");
+}
